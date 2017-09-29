b6f67d487fc041fc7dba0029be2e50db29a4a29e
Darnell Malone <dmalone08@me.com>
2017-09-29 15:14:38 -0500

    Add new map for itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Darnell Malone on 9/29/17.
+//  Copyright © 2017 Darnell Malone. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
